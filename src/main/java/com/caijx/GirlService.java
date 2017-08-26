19003969718e6ee6d9bfa863f3372d690fe16f37
SmileCJX <1092925421@qq.com>
2017-08-26 23:21:21 +0800

    事务管理

---
+++ src/main/java/com/caijx/GirlService.java
@@ -0,0 +1,29 @@
+package com.caijx;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by Administrator on 2017/8/26/026.
+ */
+@Service
+public class GirlService {
+
+    @Autowired
+    private GirlRepository girlRepository;
+
+    @Transactional
+    public void insertTwo(){
+        Girl girlA = new Girl();
+        girlA.setCupSize("A");
+        girlA.setAge(18);
+        girlRepository.save(girlA);
+
+        Girl girlB = new Girl();
+        girlB.setCupSize("BBBB");
+        girlB.setAge(19);
+        girlRepository.save(girlB);
+
+    }
+}
